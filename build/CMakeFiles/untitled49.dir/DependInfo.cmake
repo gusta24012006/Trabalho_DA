bc7967884b3bb97a9897175c00070f4f05c44995
Gustavo Monteiro <up202405575@edu.fe.up.pt>
2026-03-18 10:24:01 +0000

    main.cpp e assigner.cpp

---
+++ build/CMakeFiles/untitled49.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/gusta/OneDrive/Ambiente de Trabalho/DA/Assigner.cpp" "CMakeFiles/untitled49.dir/Assigner.cpp.obj" "gcc" "CMakeFiles/untitled49.dir/Assigner.cpp.obj.d"
+  "C:/Users/gusta/OneDrive/Ambiente de Trabalho/DA/CsvReader.cpp" "CMakeFiles/untitled49.dir/CsvReader.cpp.obj" "gcc" "CMakeFiles/untitled49.dir/CsvReader.cpp.obj.d"
+  "C:/Users/gusta/OneDrive/Ambiente de Trabalho/DA/graph.cpp" "CMakeFiles/untitled49.dir/graph.cpp.obj" "gcc" "CMakeFiles/untitled49.dir/graph.cpp.obj.d"
+  "C:/Users/gusta/OneDrive/Ambiente de Trabalho/DA/main.cpp" "CMakeFiles/untitled49.dir/main.cpp.obj" "gcc" "CMakeFiles/untitled49.dir/main.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/untitled49.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/untitled49.dir/Assigner.cpp.obj"
+  "CMakeFiles/untitled49.dir/Assigner.cpp.obj.d"
+  "CMakeFiles/untitled49.dir/CsvReader.cpp.obj"
+  "CMakeFiles/untitled49.dir/CsvReader.cpp.obj.d"
+  "CMakeFiles/untitled49.dir/graph.cpp.obj"
+  "CMakeFiles/untitled49.dir/graph.cpp.obj.d"
+  "CMakeFiles/untitled49.dir/main.cpp.obj"
+  "CMakeFiles/untitled49.dir/main.cpp.obj.d"
+  "libuntitled49.dll.a"
+  "untitled49.exe"
+  "untitled49.exe.manifest"
+  "untitled49.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/untitled49.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
